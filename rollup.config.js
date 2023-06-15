1d200712be2cc6ed7df217993724a79335b54cd5
hectorhli <hectorhli@tencent.com>
2023-06-15 16:07:34 +0800

    build

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@
 export default {
 	input: 'compiler/index.js',
 	// moduleName: 'svelte',
-	output: [{file:'dist/svelte.es.js',format:'es'},{file:'dist/svelte.umd.js',format:'umd'}],
+	output: [{file:'dist/svelte.es.js',format:'es'}],
 	// plugins: [
 	// 	nodeResolve({ jsnext: true, module: true })
 	// ]
