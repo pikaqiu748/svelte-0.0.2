9d469129dd403bc1b6730865a8f25e0fb2afa757
hectorhli <hectorhli@tencent.com>
2023-06-17 18:09:24 +0800

    添加注释

--- compiler/index.js
+++ compiler/index.js
@@ -2,6 +2,12 @@ import parse from './parse/index.js';
 import generate from './generate/index.js';
 
 export function compile ( template ) {
+	// parse()返回的内容
+	// return {
+	// 	html: parser.html,
+	// 	css: parser.css,
+	// 	js: parser.js,
+	//   }
 	const parsed = parse( template );
 	// TODO validate template
 	const generated = generate( parsed, template );
