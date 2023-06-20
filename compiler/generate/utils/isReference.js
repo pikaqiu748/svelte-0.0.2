eebd98216444bbf9d080a0f4b115428d40504f47
hectorhli <hectorhli@tencent.com>
2023-06-20 16:15:44 +0800

    添加注释

--- compiler/generate/utils/isReference.js
+++ compiler/generate/utils/isReference.js
@@ -1,11 +1,25 @@
 //  binding-input-text-deep-contextual文件为例
 //  第一个参数：Node { type: 'Identifier', start: 8, end: 13, name: 'items' },
 
+// 方括号里的 key
+// > cd = `dict[key]`
+// > parse(cd).body[0]
+// Node {
+//   type: 'ExpressionStatement',
+//   expression: Node {
+//     type: 'MemberExpression',
+//     object: Node { type: 'Identifier', name: 'dict' },
+//     computed: true,
+//     property: Node { type: 'Identifier', name: 'key' }
+//   }
+// }
+
 export default function isReference(node, parent) {
   if (node.type === 'MemberExpression') {
+    // MemberExpression 表示 a.b或者a[b]这样的语句，第一种方式computed为false,第二种computed为true
     return !node.computed && isReference(node.object, node)
   }
-
+  // 标识符
   if (node.type === 'Identifier') {
     // the only time we could have an identifier node without a parent is
     // if it's the entire body of a function without a block statement –
@@ -16,6 +30,19 @@ export default function isReference(node, parent) {
     if (parent.type === 'MemberExpression' || parent.type === 'MethodDefinition') {
       return parent.computed || node === parent.object
     }
+
+    //  对象表达式中的属性节点。key 表示键，value 表示值，由于 ES5 语法中有 get/set 的存在，所以有一个 kind 属性，用来表示是普通的初始化，或者是 get/set。
+    // interface Property <: Node {
+
+    //     type: "Property";
+
+    //     key: Literal | Identifier;
+
+    //     value: Expression;
+
+    //     kind: "init" | "get" | "set";
+
+    // }
 
     // disregard the `bar` in `{ bar: foo }`, but keep it in `{ [bar]: foo }`
     if (parent.type === 'Property') return parent.computed || node === parent.value
